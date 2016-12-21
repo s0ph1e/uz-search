68ec815d7ae496059db4dde474624cf438459c3d
s0ph1e <sonya1912@gmail.com>
2016-12-21 11:46:07 +0200

    Use es6 in step edfinitions, save screenshot for all trains

--- features/step_definitions/browser_steps.js
+++ features/step_definitions/browser_steps.js
@@ -1,6 +1,6 @@
-var seleniumWebdriver = require('selenium-webdriver');
-var until = seleniumWebdriver.until;
-var By = seleniumWebdriver.By;
+let seleniumWebdriver = require('selenium-webdriver');
+let until = seleniumWebdriver.until;
+let By = seleniumWebdriver.By;
 
 function stepDefinitions() {
     this.Given('I am on the UZ booking site', function () {
@@ -8,24 +8,20 @@ function stepDefinitions() {
     });
 
     this.When('I choose departure station {station}', function (station) {
-        var driver = this.driver;
-
-        return driver.findElement(By.id('station_from')).then(function (element) {
-            return element.findElement(By.name('station_from')).sendKeys(station).then(function () {
-                return element.findElement(By.id('stations_from')).then(function (dropdownElement) {
-                    return driver.wait(until.elementIsVisible(dropdownElement), 1000).findElement(By.tagName('div')).click();
+        return this.driver.findElement(By.id('station_from')).then((element) => {
+            return element.findElement(By.name('station_from')).sendKeys(station).then(() => {
+                return element.findElement(By.id('stations_from')).then((dropdownElement) => {
+                    return this.driver.wait(until.elementIsVisible(dropdownElement), 1000).findElement(By.tagName('div')).click();
                 });
             });
         });
     });
 
     this.When('I choose arrival station {station}', function (station) {
-        var driver = this.driver;
-
-        return driver.findElement(By.id('station_till')).then(function (element) {
-            return element.findElement(By.name('station_till')).sendKeys(station).then(function () {
-                return element.findElement(By.id('stations_till')).then(function (dropdownElement) {
-                    return driver.wait(until.elementIsVisible(dropdownElement), 1000).findElement(By.tagName('div')).click();
+        return this.driver.findElement(By.id('station_till')).then((element) => {
+            return element.findElement(By.name('station_till')).sendKeys(station).then(() => {
+                return element.findElement(By.id('stations_till')).then((dropdownElement) => {
+                    return this.driver.wait(until.elementIsVisible(dropdownElement), 1000).findElement(By.tagName('div')).click();
                 });
             });
         });
@@ -40,14 +36,13 @@ function stepDefinitions() {
     });
 
     this.Then('I should see available trains', function () {
-        var driver = this.driver;
-        return this.driver.findElement(By.id('ts_res')).then(function (resultTableElement) {
-            return driver.wait(until.elementIsVisible(resultTableElement), 5000)
-        })
+        return this.driver.findElement(By.id('ts_res')).then((resultTableElement) => {
+            return this.driver.wait(until.elementIsVisible(resultTableElement), 5000).then(this.saveScreenshot);
+        });
     });
 
     this.Then('I should see available train {number}', function (number) {
-        var xpath = "//td[contains(@class, 'num')]/a[contains(text(),'" + number + "')]";
+        let xpath = "//td[contains(@class, 'num')]/a[contains(text(),'" + number + "')]";
         return this.driver.wait(until.elementLocated({xpath: xpath}), 5000).then(this.saveScreenshot);
     });
 }
--- features/uz-search.feature
+++ features/uz-search.feature
@@ -6,4 +6,5 @@ Feature: UZ search
     And I choose arrival station Ивано-Франковск
     And I choose date 13.01.2017
     And I do search
-    Then I should see available train 043
+    Then I should see available trains
+    #Then I should see available train 043
